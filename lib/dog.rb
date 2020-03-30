3f2ce6abd2a87a38029ceaa4927d25e687b2a983
Daniel Kersten <dkersten1994@gmail.com>
2020-03-30 20:07:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog
-  def initialize(name)
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  def name
     @name = name
   end
   
-  def breed=(breed)
-    @breed = breed
-  end
- 
   def breed
-    @breed
+    @breed = breed
   end
 end
