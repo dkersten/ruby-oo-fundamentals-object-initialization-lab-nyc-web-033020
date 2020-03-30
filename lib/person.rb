6186fc877293d42ac7c711807315df6c54733e27
Daniel Kersten <dkersten1994@gmail.com>
2020-03-30 20:07:05 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,15 @@
 class Person
   def initialize(name, breed="Mutt")
     @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name = name
+  end
+  
+  def breed
+    @breed = breed
   end
 end
 
